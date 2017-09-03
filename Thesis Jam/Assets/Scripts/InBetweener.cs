92d4bc7066310b2ee95cf7fed7e8dafa3d897c4e
Robert Rappoport <rorappo@gmail.com>
2017-09-02 23:42:55 -0400

    Secondarily Init Commit

---
+++ Thesis Jam/Assets/Scripts/InBetweener.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InBetweener : MonoBehaviour {
+
+	public GameObject object1;
+	public GameObject object2;
+	void Update()
+	{
+		this.transform.position = 0.5f*(object1.transform.position + object2.transform.position);
+	}
+}
