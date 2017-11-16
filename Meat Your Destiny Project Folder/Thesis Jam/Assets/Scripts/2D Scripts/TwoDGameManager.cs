00bf077abe5041e45b333646d97f4d9f9f926155
robrappoport <rjr363@nyu.edu>
2017-11-16 15:38:30 -0500

    score system implemented

--- Meat Your Destiny Project Folder/Thesis Jam/Assets/Scripts/2D Scripts/TwoDGameManager.cs
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/Scripts/2D Scripts/TwoDGameManager.cs
@@ -7,31 +7,61 @@ using UnityEngine.UI;
 
 
 public class TwoDGameManager : MonoBehaviour {
+    public static TwoDGameManager thisInstance;
 	public auraPlayerHealth playerHealth1;
 	public auraPlayerHealth playerHealth2;
     const int maxPlayers = 2;
 	public auraGunBehavior[] players;
 	private int restartTime = 3;
 	public Text playerWinner;
+    public static Text player1Score;
+    public static int player1ScoreNum = 0;
+    public static Text player2Score;
+    public static int player2ScoreNum = 0;
+    private bool addedScore = false;
 //	GameObject audioManagerClone;
 //	public GameObject audioManagerPrefab;
 
+    void OnApplicationQuit()
+    {
+        thisInstance = null;
+    }
 	void Awake ()
 	{
+        if (thisInstance == null)
+        {
+            thisInstance = GameObject.Find("gameManager").GetComponent<TwoDGameManager>();
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+        player1Score = GameObject.Find("Player1Score").GetComponent<Text>();
+        player2Score = GameObject.Find("Player2Score").GetComponent<Text>();
 		playerWinner.text = " ";
 
 //		if (!GameObject.Find ("AudioManager(Clone)")) {
 //			audioManagerClone = Instantiate (audioManagerPrefab);
 //		}
 	}
+
 	void Update ()
 	{
+        playerScoreUpdate();
 		if (playerHealth1.CurrentHealth <= 0 || playerHealth2.CurrentHealth <= 0) {
-			StartCoroutine (gameRestart());
-			if (playerHealth1.CurrentHealth > 0) {
-				playerWinner.text = "player one wins";
-			} else {
-				playerWinner.text = "player two wins";
+            if (playerHealth1.CurrentHealth > 0 && addedScore == false) {
+                addedScore = true;
+                player1ScoreNum ++;
+				playerWinner.text = "green wins";
+                StartCoroutine(gameRestart());
+                return;
+            } 
+            if (playerHealth2.CurrentHealth > 0 && addedScore == false){
+                addedScore = true;
+                player2ScoreNum++;
+				playerWinner.text = "red wins";
+                StartCoroutine(gameRestart());
+                return;
 			}
 		}
 
@@ -41,5 +71,12 @@ public class TwoDGameManager : MonoBehaviour {
 	{
 		yield return new WaitForSeconds (restartTime);
 		SceneManager.LoadScene("AuraVersion");
+        addedScore = false;
 	}
+
+    public void playerScoreUpdate ()
+    {
+        player1Score.text = "green divine ascensions\n" + player1ScoreNum;
+        player2Score.text = "red divine ascensions\n" + player2ScoreNum;
+    }
 }
