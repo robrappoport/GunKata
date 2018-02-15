c876bdb48c168a70a653ff2ec4313c6420f7903f
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-02-15 13:57:17 -0500

    Mesh deformation in (bullet entering aura needs work, settings need to be adjusted)

---
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/MeshDeformerDetector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshDeformerDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		print ("getting collision");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
