459a1a6e9ac80a4c04d91a023ce654fe5a301a15
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-02-15 14:28:22 -0500

    Level select screen added and loaded to build settings (defaults to aura version for invalid scenes)

---
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/LevelSelect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using InControl;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+	public static PlayControl instance;
+	public InputDevice myController;
+
+	public Text[] levels;
+	public GameObject controls;
+	public int selectedText;//0 is start, 1 is controls
+	public bool controlsActive, leftStickHeld;
+	// Use this for initialization
+	void Start () {
+		selectedText = 0;
+		controlsActive = false;
+		myController = InputManager.Devices [0];
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	
+		for (int i =0; i < levels.Length; i++) {
+			if (i == selectedText) {
+				levels [i].color = Color.white;
+			}
+				else{
+					levels[i].color = Color.black;
+				}
+			}
+
+
+		if(myController.AnyButtonWasReleased){
+
+			if(Application.CanStreamedLevelBeLoaded( levels[selectedText].name)){
+				SceneManager.LoadScene (levels [selectedText].name);	
+			
+			} else {
+				SceneManager.LoadScene ("AuraVersion");
+
+			}
+		}
+
+		if (!controlsActive) {
+			if (!leftStickHeld) {
+				if (myController.LeftStick.Y < 0) {
+							selectedText = Mathf.Clamp (selectedText + 1, 0, levels.Length);
+					leftStickHeld = true;
+				} else if (myController.LeftStick.Y > 0) {
+							selectedText = Mathf.Clamp (selectedText - 1, 0, levels.Length);
+					leftStickHeld = true;
+
+				}
+			}
+		}
+		if (myController.LeftStick.Y == 0) {
+			leftStickHeld = false;
+		}
+	}
+}
