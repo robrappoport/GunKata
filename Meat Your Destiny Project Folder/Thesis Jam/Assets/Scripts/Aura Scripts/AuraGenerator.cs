98921d31c8c567a1d4ff908fdce0768b062a2200
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-02-19 20:38:39 -0500

    Deformation more customizable; may need debugging or refactoring for smoothness

--- Meat Your Destiny Project Folder/Thesis Jam/Assets/Scripts/Aura Scripts/AuraGenerator.cs
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/Scripts/Aura Scripts/AuraGenerator.cs
@@ -17,11 +17,60 @@ public class AuraGenerator : MonoBehaviour {
 	void Start () {
         
 	}
-	
+
+	void DeformMultiple(Collider col, int totalPoints = 6, float radiusFactor = 1, int iterations = 1){
+		bool firstIteration;
+		for(int i = 0; i<iterations; i++){
+			if (i == 0) {
+				firstIteration = true;
+			} else {
+				firstIteration = false;
+			}
+			Deform (col, totalPoints, radiusFactor / iterations * i, firstIteration);
+		}
+	}
+
+	void Deform(Collider col, int totalPoints = 6, float radiusFactor = 1, bool firstIteration = true){
+		Vector3 point = GetComponent<Collider> ().ClosestPointOnBounds (col.transform.position);
+		List<Vector3> points = new List<Vector3> ();
+		List<Vector3> hits = new List<Vector3> ();
+		if (firstIteration) {
+			points.Add (point);
+		}
+
+
+//		GameObject temp = new GameObject ("temp");
+//		temp.transform.position = point;
+//		temp.transform.LookAt (transform, transform.up);
+		for (int i = 0; i <totalPoints; i++) {
+			float l;
+			if (col.GetComponent<SphereCollider>()){
+				l = col.GetComponent<SphereCollider>().radius * radiusFactor;
+			}else{
+				l = col.bounds.size.x;
+			}
+				
+			Vector3 newPoint = point + Quaternion.AngleAxis (360 / totalPoints * i, (transform.position - point) / Vector3.Distance (transform.position, point)) * (Vector3.right * l);
+			points.Add (newPoint);
+		}
+		for (int i = 0; i < points.Count; i++) {
+			RaycastHit hit;
+			if (Physics.Raycast (points [i], (transform.position - col.transform.position) / Vector3.Distance (transform.position, col.transform.position), out hit,
+				Vector3.Distance (transform.position, col.transform.position), ~LayerMask.NameToLayer("Aura"), QueryTriggerInteraction.Collide)) {
+				hits.Add (hit.point);
+			}
+		}
+		for (int i = 0; i < hits.Count; i++) {
+
+			GetComponent<MeshDeformer> ().AddDeformingForce (hits[i],
+				
+				Mathf.Clamp(col.GetComponent<Rigidbody>().velocity.magnitude *  Vector3.Distance (hits[i], col.ClosestPoint(hits[i])) * deformingForceModifier, 0, deformingLimit ));
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-		if (auraCurLife >0.5f && !GetComponent<MeshDeformer> ()) {
-			gameObject.AddComponent<MeshDeformer> ();
+		if (auraCurLife >0.1f && !GetComponent<MeshDeformer> ()) {
+			gameObject.GetComponent<MeshDeformer> ().enabled = true;
 			GetComponent<MeshDeformer> ().damping = 2;
 		}
         auraCurLife = Mathf.Clamp(auraCurLife, 0, auraLifeTime);
@@ -46,21 +95,25 @@ public class AuraGenerator : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider col){
-		Deform (col);
+		if (GetComponent<MeshDeformer> () && col.GetComponent<Rigidbody>()) {
+			Deform (col, 6, .5f);
+		}
 
 		//		MakeParticles (col, true);
 	}
 	void OnTriggerExit(Collider col){
 //		MakeParticles (col, false);
-		Deform(col);
+		if (GetComponent<MeshDeformer> () && col.GetComponent<Rigidbody>()) {
+			Deform (col, 6, .5f);
+		}
 	
 	}
 
-	void Deform(Collider col){
-		if (GetComponent<MeshDeformer> ()) {
-			GetComponent<MeshDeformer> ().AddDeformingForce (GetComponent<Collider> ().ClosestPointOnBounds (col.transform.position), -Mathf.Clamp(GetComponent<Rigidbody>().velocity.magnitude * deformingForceModifier, 5000, deformingLimit ));
-		}
-	}
+//	void Deform(Collider col){
+//		if (GetComponent<MeshDeformer> ()) {
+//			GetComponent<MeshDeformer> ().AddDeformingForce (GetComponent<Collider> ().ClosestPointOnBounds (col.transform.position), -Mathf.Clamp(GetComponent<Rigidbody>().velocity.magnitude * deformingForceModifier, 5000, deformingLimit ));
+//		}
+//	}
 	void MakeParticles(Collider col, bool entering){
 		if (col.GetComponent<Bullet>() || col.GetComponent<Cannonball> ()) {
 			
