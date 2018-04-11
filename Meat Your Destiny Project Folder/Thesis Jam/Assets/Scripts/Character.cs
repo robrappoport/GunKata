567d7cf779893f9a9c9c5de81b04cd26f052d024
ANUBIS\Jehuty <juan.irizarry.cole@gmail.com>
2018-04-11 18:02:01 -0400

    Groundwork laid for character scriptable object

---
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/Scripts/Character.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCharacter", menuName = "Character")]
+public class Character:ScriptableObject {
+	[Header("General vars")]
+	public float staminaRechargeRate;
+	[Header("Movement vars")]
+	public float TurnSpeed;
+	public float PrevMoveForce, SlowForce, shootForce, moveMultipler, stuckTime, forceMultiplier, dashCost, dashStaminaDrainRate;
+
+	[Header("Aura charge vars")]
+	public float auraDrainRate;
+	public float auraChargeRate, coolDownDuration;
+	[Header("Charge shot vars")]
+	public float laserStaminaDrainRate;
+	public float initialChargeBuffer, LoadedChargeTime, TotalLaserShotTime;
+
+
+}
