c809d45c79d8fbf494a57a72c95ed7f5724ac16a
robrappoport <rjr363@nyu.edu>
2018-04-26 16:31:35 -0400

    space level almost set up

---
+++ Meat Your Destiny Project Folder/Thesis Jam/Assets/MeshOffset.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshOffset : MonoBehaviour {
+
+    public Vector2 scrollVelocity;
+
+    private Renderer _renderer;
+
+    public bool useUnscaledtime = true;
+	// Use this for initialization
+	void Start () {
+        _renderer = GetComponent<Renderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (useUnscaledtime)
+        {
+            _renderer.sharedMaterial.mainTextureOffset += scrollVelocity * Time.unscaledDeltaTime;
+        }
+        else
+        {
+            _renderer.sharedMaterial.mainTextureOffset += scrollVelocity * Time.deltaTime;
+        }
+
+		
+	}
+}
